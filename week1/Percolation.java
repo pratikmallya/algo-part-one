21b28aea6501f29630ffcd2fc2ac464e15000aaa
Pratik Mallya <pratik.mallya@gmail.com>
2016-02-28 12:24:09 -0600

    Performance improvements
    I had not noticed the performance specifications in the problem.
    These changes ensure that those are met, by elliminating looping
    and making use of the UF structures find() method.

--- week1/Percolation.java
+++ week1/Percolation.java
@@ -5,6 +5,7 @@ public class Percolation {
   private int N;
   private boolean[][] Grid;
   private WeightedQuickUnionUF QF;
+  private boolean isPercolate;
 
   public Percolation(int N) {
     if (N <= 0) {
@@ -14,6 +15,7 @@ public class Percolation {
     this.Grid = new boolean[N][N];
     this.UF = new WeightedQuickUnionUF(N);
     this.N = N;
+    this.isPercolate = false;
   };
 
   public void open(int i, int j) {
@@ -21,15 +23,20 @@ public class Percolation {
       String msg = String.format("i and j must both lie in 1 <= %d, but got i=%d and j=%d", this.N, i, j);
       throw new java.lang.IndexOutOfBoundsException(msg);
     }
+    // open up the site
     this.Grid[i][j] = true;
     // update UF structure with neighbor information
     int[] indexList = {-1, 1};
     for (int p : indexList){
       for(int q : indexList){
         if (1 <= p && p<= this.N && 1 <= q && q <= this.N && this.isOpen(p, q)){
-          this.UF.union(this.flattenMatrix(i,j), this.flattenMatrix(p,q));
+          this.UF.union(this.collapseDimension(i,j), this.collapseDimension(p,q));
         }
       }
+    }
+    // if the newly opened site is full, the system percolates
+    if (this.isFull(i, j)) {
+      this.isPercolate = true;
     }
   };
 
@@ -46,27 +53,28 @@ public class Percolation {
       String msg = String.format("i and j must both lie in 1 <= %d, but got i=%d and j=%d", this.N, i, j);
       throw new java.lang.IndexOutOfBoundsException(msg);
     }
-    // check if given point is in the same component as an open site on top row
-    for (int p=1; p<=this.N; p++) {
-      if (this.isOpen(1, p) && this.UF.connected(this.flattenMatrix(i, j), this.flattenMatrix(1, p))){
-        return true;
-      }
+    if (!this.isOpen(i, j)) {
+      return false;
     }
-    return false;
+    // check if the component that this site belongs to is open
+    int[] compIndex = this.expandDimension(this.UF.find(this.collapseDimension(i, j)));
+    return this.isOpen(compIndex[0], compIndex[1]);
   };
 
   public boolean percolates(){
-    for (int i=1; i <=this.N; i++) {
-      if (this.isFull(this.N, i)) {
-        return true;
-      };
-    };
-    return false;
+    return this.isPercolate;
   };
 
   public static void main(String [] args){};
 
-  private int flattenMatrix(int i, int j) {
+  private int collapseDimension(int i, int j) {
     return (i-1)*this.N + j;
+  }
+
+  private int[] expandDimension(int U) {
+    int i = U/this.N + 1;
+    int j = U - this.N * (i-1);
+    int[] retarr = {i, j};
+    return retarr;
   }
 }
